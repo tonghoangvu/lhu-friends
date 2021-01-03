fb49921d87d3b2c4e74c3571b5d4d908482f6100
tonghoangvu <tonghoangvu@outlook.com>
2021-01-03 13:16:14 +0700

    Khởi tạo Spring Boot project

---
+++ src/main/java/com/tonghoangvu/lhufriends/LhuFriendsApplication.java
@@ -0,0 +1,11 @@
+package com.tonghoangvu.lhufriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhuFriendsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LhuFriendsApplication.class, args);
+    }
+}
