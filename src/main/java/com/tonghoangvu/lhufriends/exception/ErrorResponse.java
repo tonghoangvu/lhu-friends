5f0da0bd5de3c60f98a0c56657748db0067a4dc5
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-06-25 21:50:39 +0700

    Rút gọn khởi tạo ErrorResponse

--- src/main/java/com/tonghoangvu/lhufriends/exception/ErrorResponse.java
+++ src/main/java/com/tonghoangvu/lhufriends/exception/ErrorResponse.java
@@ -1,12 +1,13 @@
 package com.tonghoangvu.lhufriends.exception;
 
 import com.tonghoangvu.lhufriends.common.ErrorCode;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
-@Builder
+@AllArgsConstructor
 public class ErrorResponse {
     private final @NotNull ErrorCode code;
     private final @NotNull String message;
--- src/main/java/com/tonghoangvu/lhufriends/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tonghoangvu/lhufriends/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.tonghoangvu.lhufriends.exception;
 
 import com.tonghoangvu.lhufriends.common.ErrorCode;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,50 +14,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(AppException.class)
     protected @NotNull ResponseEntity<ErrorResponse> handleAppException(@NotNull AppException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(e.getErrorCode())
-                .message(e.getMessage())  // Managed exceptions message
-                .build();
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getStatusCode()).body(errorResponse);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     protected @NotNull ResponseEntity<ErrorResponse> handleBadCredentialsException(
             @NotNull BadCredentialsException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(ErrorCode.BAD_CREDENTIALS)
-                .message(e.getMessage())  // Modified exception message
-                .build();
-        return ResponseEntity.status(401).body(errorResponse);
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.BAD_CREDENTIALS, e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     protected @NotNull ResponseEntity<ErrorResponse> handleAccessDeniedException(
             @NotNull AccessDeniedException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(ErrorCode.ACCESS_DENIED)
-                .message(e.getMessage())  // Modified exception message
-                .build();
-        return ResponseEntity.status(403).body(errorResponse);
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.ACCESS_DENIED, e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 
     @ExceptionHandler(LockedException.class)
     protected @NotNull ResponseEntity<ErrorResponse> handleLockedException(
             @NotNull LockedException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(ErrorCode.ACCOUNT_LOCKED)
-                .message(e.getMessage())  // Modified exception message
-                .build();
-        return ResponseEntity.status(403).body(errorResponse);
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.ACCOUNT_LOCKED, e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     protected @NotNull ResponseEntity<ErrorResponse> handleUnwantedException(@NotNull Exception e) {
         e.printStackTrace();  // Critical error, print details
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(ErrorCode.SERVER_ERROR)
-                .message("Internal Server Error")  // Don't send exception message to client
-                .build();
-        return ResponseEntity.status(500).body(errorResponse);
+        ErrorResponse errorResponse = new ErrorResponse(
+                ErrorCode.SERVER_ERROR, "Internal Server Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
